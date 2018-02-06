c420524013ffe5d36c8ff9242e920ac62a382e59
suzygd <suzy.gaidoz@gmail.com>
2018-02-06 20:25:05 +0100

    Update env.js

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 module.export = {
 
-consumer_key: 'hhDzo6eKEwcmNgVgiYEuoK48g',
-  consumer_secret: '5UGQpOkIHNmuoZ8Jr4Pt47xBlqISpkJcD2fPt3rJj8xPZERHO3',
-  access_token_key: '1009053732-fEE4MY5ktALCXfXtimH22YFzyGfGWMoBuAHOC9l',
-  access_token_secret: 'ovOqohg62KzIJgCo4BSJdzamOpKjFdjbh9Ss4NTa5qtxF'
+consumer_key: '',
+  consumer_secret: '',
+  access_token_key: 'X',
+  access_token_secret: 'X'
 };
