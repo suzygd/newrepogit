74ba1c695b6a3d88091d43c477154867d3741557
suzygd <suzy.gaidoz@gmail.com>
2018-02-06 20:09:18 +0100

    Create env.js

---
+++ env.js
@@ -0,0 +1,7 @@
+module.export = {
+
+consumer_key: 'hhDzo6eKEwcmNgVgiYEuoK48g',
+  consumer_secret: '5UGQpOkIHNmuoZ8Jr4Pt47xBlqISpkJcD2fPt3rJj8xPZERHO3',
+  access_token_key: '1009053732-fEE4MY5ktALCXfXtimH22YFzyGfGWMoBuAHOC9l',
+  access_token_secret: 'ovOqohg62KzIJgCo4BSJdzamOpKjFdjbh9Ss4NTa5qtxF'
+};
